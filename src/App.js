3d8935e21cdc47572e836320885dbf75c83a5023
BoyPhanna <kimphanna56@gmail.com>
2023-10-22 13:57:51 +0700

    Functional Component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
-import { useState } from "react";
+import Header from "./components/Header";
+import StudentList from "./components/StudentList";
 function App() {
-// បង្កើត 
-const [student,setStudent]=useState([
-{id:1,name:"Phanna"},
-{id:2,name:"JoJo"},
-{id:3,name:"Putina"},
-])
-const [show,setShow]=useState(true);
-function deleteStudent(id){
-  setStudent(student.filter(item=>item.id !==id))
-}
 return (
     <>
-      <h1>ចំនួនសិស្ស : {student.length}</h1>
-      <button onClick={()=>setShow(!show)}>togle</button>
-      <ul>
-        {show && student.map((item)=>(
-          <li key={item.id}>
-            <p>{item.id} - {item.name}</p>
-            <button onClick={()=>deleteStudent(item.id)}>Delelte</button>
-          </li>
-        ))}
-      </ul>
+      <Header/>
+      <StudentList></StudentList>
     </>
   );
 }
---
+++ src/components/StudentList.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+export default function StudentList(){
+// បង្កើត 
+const [student,setStudent]=useState([
+    {id:1,name:"Phanna"},
+    {id:2,name:"JoJo"},
+    {id:3,name:"Putina"},
+    ])
+    const [show,setShow]=useState(true);
+    function deleteStudent(id){
+      setStudent(student.filter(item=>item.id !==id))
+    }
+    return (
+        <>
+        <h1>ចំនួនសិស្ស : {student.length}</h1>
+        <button onClick={() => setShow(!show)}>togle</button><ul>
+            {show && student.map((item) => (
+                <li key={item.id}>
+                    <p>{item.id} - {item.name}</p>
+                    <button onClick={() => deleteStudent(item.id)}>Delelte</button>
+                </li>
+            ))}
+        </ul>
+    </>
+    );
+}
